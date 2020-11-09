49a2228215536fc977950a794bd5dda6bffb4ccc
chrzyki <christoph@functor-argument.net>
2020-11-09 10:13:12 +0100

    Re-generate CLDF, update code, update metadata

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # CLDF dataset derived from Kleinewillinghöfer's "Bikwin-Jen Comparative Wordlist" from 2015
 
+This dataset is licensed under a CC-BY-4.0 license
+
+Available online at https://www.blogs.uni-mainz.de/fb07-adamawa/adamawa-languages/bikwin-jen-group/
+
 
 Conceptlists in Concepticon:
 - [Kleinewillinghoefer-2015-119](https://concepticon.clld.org/contributions/Kleinewillinghoefer-2015-119)
@@ -7,7 +11,7 @@ Conceptlists in Concepticon:
 ## Statistics
 
 
-[![Build Status](https://travis-ci.org/lexibank/Kleinewillinghoeferbikwinjen.svg?branch=master)](https://travis-ci.org/lexibank/Kleinewillinghoeferbikwinjen)
+[![Build Status](https://travis-ci.org/lexibank/kleinewillinghoeferbikwinjen.svg?branch=master)](https://travis-ci.org/lexibank/kleinewillinghoeferbikwinjen)
 ![Glottolog: 92%](https://img.shields.io/badge/Glottolog-92%25-green.svg "Glottolog: 92%")
 ![Concepticon: 99%](https://img.shields.io/badge/Concepticon-99%25-brightgreen.svg "Concepticon: 99%")
 ![Source: 100%](https://img.shields.io/badge/Source-100%25-brightgreen.svg "Source: 100%")
@@ -17,3 +21,13 @@ Conceptlists in Concepticon:
 - **Lexemes:** 1,804
 - **Sources:** 2
 - **Synonymy:** 1.16
+
+# Contributors
+
+Name | GitHub user | Description | Role
+--- | --- | --- | ---
+Ulrich Kleinewillinghöfer | | data collection | DataCollector
+Kristina-Pianykh | @Kristina-Pianykh  | initial code | Author
+Christoph Rzymski | @chrzyki  | maintainer | Author
+
+
--- lexibank_kleinewillinghoeferbikwinjen.py
+++ lexibank_kleinewillinghoeferbikwinjen.py
@@ -7,67 +7,68 @@ from clldutils.misc import slug
 
 @attr.s
 class CustomLanguage(pylexibank.Language):
-	Source = attr.ib(default=None)
+    Source = attr.ib(default=None)
 
 
 class Dataset(pylexibank.Dataset):
-	dir = Path(__file__).parent
-	id = "kleinewillinghoeferbikwinjen"
-	language_class = CustomLanguage
+    dir = Path(__file__).parent
+    id = "kleinewillinghoeferbikwinjen"
+    language_class = CustomLanguage
 
-	form_spec = pylexibank.FormSpec(
-		brackets={"(": ")"},
-		separators=(";", "/", ",", "~", "("),
-		missing_data=('', ' '),
-		strip_inside_brackets=True
-	)
+    form_spec = pylexibank.FormSpec(
+        brackets={"(": ")"},
+        separators=(";", "/", ",", "~", "("),
+        missing_data=("", " "),
+        strip_inside_brackets=True,
+    )
 
+    def cmd_makecldf(self, args):
+        data = []
 
-	def cmd_makecldf(self, args):
-		data = []
+        concepts = args.writer.add_concepts(
+            id_factory=lambda c: c.id[:24].replace("-", "")
+            + "_"
+            + c.id.split("-")[-1]
+            + "_"
+            + slug(c.english),
+            lookup_factory="Name",
+        )
 
-		concepts = args.writer.add_concepts(
-			id_factory=lambda c: c.id[:24].replace("-", "") + "_" + c.id.split("-")[-1] + "_" + slug(c.english), lookup_factory="Name"
-		)
+        languages = args.writer.add_languages(lookup_factory="Name")
+        args.writer.add_sources()
 
-		languages = args.writer.add_languages(lookup_factory="Name")
-		args.writer.add_sources()
+        comments = {
+            "fɛɛni; tshwɐnì (little thing)": "little thing",
+            "tswáni; feenì (very small)": "very small",
+            "jì(ì) (=calabash)": "=calabash",
+            "wáà (swín (~pots)": "~pots",
+            "wuo wo; tá (go)": "go",
+            "wowə́, wo (sit down)": "sit down",
+            "ɗò kàà déé, wuri ka déé (~up)": "~up",
+            "e mí (give me)": "give me",
+            "ŋə nuŋ (-sth.)": "-sth.",
+        }
 
-		comments = {
-		'fɛɛni; tshwɐnì (little thing)': 'little thing',
-		'tswáni; feenì (very small)': 'very small',
-		'jì(ì) (=calabash)': '=calabash',
-		'wáà (swín (~pots)': '~pots',
-		'wuo wo; tá (go)': 'go',
-		'wowə́, wo (sit down)': 'sit down',
-		'ɗò kàà déé, wuri ka déé (~up)': '~up',
-		'e mí (give me)': 'give me',
-		'ŋə nuŋ (-sth.)': '-sth.'
-		}
+        for i in ["Wordlist.tsv", "Pronouns and numbers.tsv"]:
+            with open(self.dir.joinpath("raw", i).as_posix(), encoding="utf-8") as csvfile:
+                reader = csv.DictReader(csvfile, delimiter="\t")
+                raw_lexemes = []
+                if i == "Wordlist.tsv":
+                    source = self.conceptlists[0].refs[0]
+                else:
+                    source = self.conceptlists[1].refs[0]
+                for row in reader:
+                    row["Source"] = source
+                    raw_lexemes.append(row)
+            data.extend(raw_lexemes)
 
-
-		for i in ["Wordlist.tsv", "Pronouns and numbers.tsv"]:
-			with open(self.dir.joinpath("raw", i).as_posix(), encoding="utf-8") as csvfile:
-				reader = csv.DictReader(csvfile, delimiter="\t")
-				raw_lexemes = []
-				if i == "Wordlist.tsv":
-					source = self.conceptlists[0].refs[0]
-				else:
-					source = self.conceptlists[1].refs[0]
-				for row in reader:
-					row['Source'] = source
-					raw_lexemes.append(row)
-			data.extend(raw_lexemes)
-
-
-		for row in pylexibank.progressbar(data):
-			for language, lexeme in row.items():
-				if language in languages:
-					print(lexeme)
-					args.writer.add_forms_from_value(
-						Language_ID=languages[language],
-						Parameter_ID=concepts[row['English']],
-						Value=lexeme,
-						Comment = comments[lexeme] if lexeme in comments.keys() else "",
-						Source=row['Source']
-					)
+        for row in pylexibank.progressbar(data):
+            for language, lexeme in row.items():
+                if language in languages:
+                    args.writer.add_forms_from_value(
+                        Language_ID=languages[language],
+                        Parameter_ID=concepts[row["English"]],
+                        Value=lexeme,
+                        Comment=comments[lexeme] if lexeme in comments.keys() else "",
+                        Source=row["Source"],
+                    )
